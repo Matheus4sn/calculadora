79af1b71ed84d94cc204605b99021cee3151c564
matheus4sn <matheus4sn@gmail.com>
2024-02-28 10:05:40 -0300

    feat: Add a control structure responsible for mathematical operations and output

--- calculadora.rb
+++ calculadora.rb
@@ -1,5 +1,27 @@
 # Asks for user input and stores in variables
 print "Digite o primeiro número: "
-num1 = gets.chomp.to_i
+num1 = gets.chomp.to_f
 print "Digite o segundo número: "
-num2 = gets.chomp.to_i
+num2 = gets.chomp.to_f
+
+# Asks for the operator
+print "Digite a operação matemática: "
+operator = gets.chomp
+
+# Control structure for math operations and output
+case operator
+when "+"
+  result = num1 + num2
+  print "O resultado é: #{result}"
+when "-"
+  result = num1 - num2
+  print "O resultado é: #{result}"
+when "*"
+  result = num1 * num2
+  print "O resultado é: #{result}"
+when "/"
+  result = num1 / num2
+  print "O resultado é: #{result}"
+else
+  puts "Operação inválida"
+end
